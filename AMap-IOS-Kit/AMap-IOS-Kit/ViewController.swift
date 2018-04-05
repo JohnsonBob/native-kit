09a4605da0a91a451d3de35868f699373f8757c5
kain <zhangtqx@vip.qq.com>
2018-04-05 18:34:42 +0800

    AMap-IOS-Kit: Develop

---
+++ AMap-IOS-Kit/AMap-IOS-Kit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AMap-IOS-Kit
+//
+//  Created by 张腾 on 2018/4/5.
+//  Copyright © 2018年 kain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AMap-IOS-Kit/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  AMap-IOS-Kit
+//
+//  Created by 张腾 on 2018/4/5.
+//  Copyright © 2018年 kain. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+
+#endif /* BridgingHeader_h */
