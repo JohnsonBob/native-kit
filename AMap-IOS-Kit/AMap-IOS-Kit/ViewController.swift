0f6a025eb97634b3ae7aa262aab7405282e0d99d
kain <zhangtqx@vip.qq.com>
2018-04-05 22:14:59 +0800

    AMap-IOS-Kit: Develop

--- AMap-IOS-Kit/AMap-IOS-Kit/ViewController.swift
+++ AMap-IOS-Kit/AMap-IOS-Kit/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController, MAMapViewDelegate {
         setAMap()
         showPositionDot()
         // updatePositionDot()
+        showsIndoorMap()
+        // showTraffic()
+        setLogoCenter()
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -60,6 +63,83 @@ class ViewController: UIViewController, MAMapViewDelegate {
         // TODO:调整定位蓝点的图标
         dot.image = UIImage(named: "mark_bs")
         amap!.update(dot)
+    }
+
+    // TODO:开启室内地图
+    private func showsIndoorMap() {
+        amap!.isShowsIndoorMap = true
+    }
+
+    // TODO:显示交通状态
+    private func showTraffic() {
+        amap!.isShowTraffic = true
+    }
+
+    // TODO:卫星地图 .standardNight=>夜景 .satellite=>卫星
+    private func setMapType() {
+        amap!.mapType = .standard
+    }
+
+    // TODO:地图Logo设置
+    private func setLogoCenter() {
+        amap!.logoCenter = CGPoint(x: 10, y: 10)
+    }
+
+    // TODO:指南针设置
+    private func setCompass() {
+        amap!.showsCompass = true
+        amap!.compassOrigin = CGPoint(x: 10, y: 10)
+    }
+
+    // TODO:比例尺设置
+    private func setScale() {
+        amap!.showsScale = true
+        amap!.scaleOrigin = CGPoint(x: 10, y: 10)
+    }
+
+    // TODO:缩放手势
+    private func setZoomEnabled() {
+        amap!.isZoomEnabled = false
+    }
+
+    // TODO:地图缩放功能拓展
+    private func setZoomLevel() {
+        amap!.setZoomLevel(17.5, animated: true)
+    }
+
+    // TODO:可以通过手势拖动地图
+    private func setScrollEnabled() {
+        amap!.isScrollEnabled = false
+    }
+
+    // TODO:地图平移式，缩放级别不变
+    private func setCenter() {
+        amap!.setCenter(CLLocationCoordinate2D(latitude: 39.920816, longitude: 116.373395), animated: true)
+    }
+
+    // TODO:禁用或开启旋转手势
+    private func setRotateEnabled() {
+        amap!.isRotateEnabled = false
+    }
+
+    // TODO:旋转扩展设置
+    private func setRotationDegree() {
+        amap!.setRotationDegree(60, animated: true, duration: 0.5)
+    }
+
+    // TODO:倾斜状态设置
+    private func setCameraEnabled() {
+        amap!.isRotateCameraEnabled = false
+    }
+
+    // TODO:倾斜扩展设置
+    private func setCameraDegree() {
+        amap!.setCameraDegree(30, animated: true, duration: 0.5)
+    }
+
+    // TODO:指定屏幕中心点的手势操作
+    private func setScreenAnchor() {
+        amap!.screenAnchor = CGPoint(x: 0.5, y: 0.8)
     }
 
     override func didReceiveMemoryWarning() {
