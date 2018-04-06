b8c21639bf911bc3e5d4dec601f799fc6185834b
kain <zhangtqx@vip.qq.com>
2018-04-07 01:01:25 +0800

    AMap-Android-Kit:Develop

--- AMap-Android-Kit-Kotlin/app/src/main/java/kain/amap_android_kit_kotlin/MainActivity.kt
+++ AMap-Android-Kit-Kotlin/app/src/main/java/kain/amap_android_kit_kotlin/MainActivity.kt
@@ -2,11 +2,36 @@ package kain.amap_android_kit_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.amap.api.maps.MapView
 
 class MainActivity : AppCompatActivity() {
+    private var amap: MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        amap = findViewById(R.id.map) as MapView
+        amap?.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        amap?.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        amap?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        amap?.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        amap?.onSaveInstanceState(outState)
     }
 }
