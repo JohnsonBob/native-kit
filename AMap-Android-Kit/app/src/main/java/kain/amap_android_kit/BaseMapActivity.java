863fcf9fda0a734c28e3c7ef3f9a08114a259064
wsy <poiuytrengo@qq.com>
2018-04-11 22:46:07 +0800

    设置地图底图语言--英语

--- AMap-Android-Kit/app/src/main/java/kain/amap_android_kit/BaseMapActivity.java
+++ AMap-Android-Kit/app/src/main/java/kain/amap_android_kit/BaseMapActivity.java
@@ -22,7 +22,10 @@ public class BaseMapActivity extends AppCompatActivity {
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
+        //设置地图底层为英语
         //aMap.setMapLanguage("en");
+
+        //设置
     }
     @Override
     protected void onDestroy() {
