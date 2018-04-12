14bd78e30699951fd5af1b352c15d27e2600a334
wsy <poiuytrengo@qq.com>
2018-04-12 22:01:44 +0800

    添加地图工具类MapOptions

--- AMap-Android-Kit/app/src/main/java/kain/amap_android_kit/MapOptions.java
+++ AMap-Android-Kit/app/src/main/java/kain/amap_android_kit/MapOptions.java
@@ -27,6 +27,7 @@ public class MapOptions {
                 Log.wtf("Msg:",myLocation.getLatitude() + " " + myLocation.getLongitude());
             }
         });
+        this.showPositionDot();
     }
 
     /**
@@ -49,7 +50,7 @@ public class MapOptions {
     /**
      * 开启默认蓝点
      */
-    public void showPositionDotitionDot(){
+    public void showPositionDot(){
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         //myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE);
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
